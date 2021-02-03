59038f4db9f17ba06c7ccadb2c2b157d04f61a5f
samimays <76924546+samimays@users.noreply.github.com>
2021-02-02 19:11:21 -0600

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,14 @@
+var test1 = null;
+
+function myFunction(){
+    if(test1 == null){
+        console.log(test1);
+        prompt()
+    }
+    
+    
+    
+    test1 = 'This worked after loading';
+    console.log(test1);
+    alert('this loaded');
+}
